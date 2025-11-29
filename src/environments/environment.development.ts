8aedda950cc8a07a450ceed1c46fa47592aa5242
ALAYA1803 <rodrigoalaya_2004@hotmail.com>
2025-11-29 17:58:06 -0500

    feat(enviroments): add new url of deployed backend

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5089/api/v1',
+  apiUrl: 'https://easyhouse-backend-gs9h.onrender.com/api/v1',
   // apiUrl: 'https://localhost:7169/api/v1'
   recaptcha: {
     siteKey: '6LeKDBwsAAAAAB7E8qZgDx1IXJ-AYd9woALqEoYR',
